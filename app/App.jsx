c3cb8ae80d2d6c123bb12d073a52c146235f8575
1102csh <121600882+1102csh@users.noreply.github.com>
2026-05-15 19:13:49 +0900

    Delete app directory

--- app/App.jsx
+++
@@ -1,8 +0,0 @@
-// src/app/App.jsx
-import { RouterProvider } from 'react-router-dom';
-//import { router } from './router';
-import { router } from '../app/router/index';
-
-const App = () => <RouterProvider router={router} />;
-
-export default App;
